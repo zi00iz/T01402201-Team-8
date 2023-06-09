426e044a544263f2c006acf77aad231c83f1d8fe
chyeon01 <86558738+chyeon01@users.noreply.github.com>
2023-06-09 23:07:00 +0900

    Add files via upload

---
+++ main/Tear2.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Tear2 : MonoBehaviour
+{
+    public void SceneChange() {
+        SceneManager.LoadScene("tear_2");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
