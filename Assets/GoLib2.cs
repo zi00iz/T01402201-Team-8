ab352d0a2c2dc28e252984df115e8bbd250c25f5
Yujin <79249718+yujin0412@users.noreply.github.com>
2023-06-09 17:49:20 +0900

    Add files via upload

---
+++ Assets/GoLib2.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoLib2 : MonoBehaviour
+{
+    public void GoToLib2()
+    {
+        SceneManager.LoadScene("lib2");
+    }
+}
